bac9803ac53635890643462f75589f84cd9eb2df
Gilberto López <gilisaac@ciencias.unam.mx>
2018-04-11 23:05:06 -0500

    El modelo se entrena y se guarda en un archivo .h5 para ser cargado después.

--- cnn.py
+++ cnn.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 # Convolutional Neural Network
 
 # Part 1 - Building the CNN
@@ -17,10 +19,10 @@ classifier.add(Conv2D(filters = 32, kernel_size = (5, 5), strides = 3,
   input_shape = (250, 250, 1), activation = 'relu'))
 
 # Step 2 - Pooling
-classifier.add(MaxPooling2D(pool_size = 3))
+classifier.add(MaxPooling2D(pool_size = 2))
 
 # Adding a second convolutional layer
-classifier.add(Conv2D(filters = 16, kernel_size = (3, 3), strides = 2,
+classifier.add(Conv2D(filters = 32, kernel_size = (3, 3), strides = 2,
   activation = 'relu'))
 classifier.add(MaxPooling2D(pool_size = 2))
 
@@ -61,30 +63,9 @@ test_set = test_datagen.flow_from_directory('dataset/test_set',
 
 classifier.fit_generator(training_set,
                          #samples_per_epoch = 8000,
-                         epochs = 5,
+                         epochs = 10,
                          validation_data = test_set)
                          #nb_val_samples = 2000)
 
-import numpy as np
-import cv2
-
-img = cv2.imread('loss_edit_1.jpg')
-small = cv2.resize(img, (250, 250))
-gray_small = cv2.cvtColor(small, cv2.COLOR_BGR2GRAY)
-
-img2 = cv2.imread('da_wae.jpg')
-small2 = cv2.resize(img2, (250, 250))
-gray_small2 = cv2.cvtColor(small2, cv2.COLOR_BGR2GRAY)
-
-# import matplotlib.pyplot as plt
-
-# plt.imshow(gray_small)
-# plt.show()
-
-#cv2.imshow('loss',gray_small)
-#cv2.imwrite('gray_small.png',gray_small)
-
-print ('Loss:',
-  classifier.predict(np.expand_dims(np.expand_dims(gray_small, axis = 2),axis=0)),
-  'Da wae:',
-  classifier.predict(np.expand_dims(np.expand_dims(gray_small2, axis = 2),axis=0)))
+# Save trained model
+classifier.save('Keras_2C32_2D64_adam_gray.h5')
